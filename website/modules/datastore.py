760fe9cd41c15df02afbf0779423e73549b2f79d
Ong Zhixian <zhixian@hotmail.com>
2020-12-05 13:21:46 +0800

    update datastore

--- website/modules/datastore.py
+++ website/modules/datastore.py
@@ -3,6 +3,7 @@
 ################################################################################
 
 import logging
+from datetime import datetime
 from uuid import uuid4
 
 from bson.objectid import ObjectId
@@ -52,7 +53,7 @@ def get_mongodb_client(database_name):
     return MongoClient(app_secrets["mongodb"][database_name])
 
 def add_message():
-    from datetime import datetime
+    
     message = {
         "author": "Mike",
         "text": "My first blog post!",
@@ -71,8 +72,46 @@ def find_message():
     message = messages.find_one({"author": "Mike"})
     return message
 
+def add_project():
+    client = get_mongodb_client('hci')
+    
+
 class hci_db():
     def __init__(self):
+        logging.info("class=hci_db method=__init__ event=begin")
         self.client = get_mongodb_client('hci')
+        self.db = self.client['hci']
+        logging.info("class=hci_db method=__init__ event=end")
+
+    def find_project(self, owner, title):
+        projects = self.db['project']
+        project = projects.find_one({"owner": {"$in":[owner]}, "title": title } )
+        return project
         
-    
+    #'PUBLIC', project_title, project_file_name, project_file_bytes
+    def add_project(self, owner, title, file_name=None, file_bytes=None, file_size=0):
+
+        logging.info("class=hci_db method=add_project event=begin")
+
+        projects = self.db['project']
+        
+        project = {
+            "title"         : title,
+            "owner"         : [owner],
+            "tags"          : ["pdf-splitter", "python", "pymongo"],
+            "filename"      : file_name,
+            "file_bytes"    : file_bytes,
+            "file_size"     : file_size,
+            "cre_dt"        : datetime.utcnow(),
+            "upd_dt"        : datetime.utcnow()
+        }
+
+        if self.find_project(owner, title) is None:
+            logging.info("proj not exist, insert")
+            project_id = projects.insert_one(project).inserted_id
+        else:
+            logging.info("exist, do nothing")
+            project_id = None
+        
+        logging.info("class=hci_db method=add_project event=end")
+        return project_id
