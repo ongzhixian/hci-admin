73cd998e65854661655258ecb5d6f031c5567927
Ong Zhixian <zhixian@hotmail.com>
2020-12-05 13:22:27 +0800

    update create project

--- website/pages/pdf_splitter.py
+++ website/pages/pdf_splitter.py
@@ -38,9 +38,37 @@ def pdf_splitter_projects_get():
 def pdf_splitter_create_project_get():
     logging.info("In pdf_splitter_create_project_get()")
     view_model = get_model()
-    # from modules.datastore import get_mongodb_client
-    # client = get_mongodb_client()
     return view(view_model)
+
+@app.route('/pdf-splitter/create-project', methods=['POST'])
+def pdf_splitter_create_project_post():
+    logging.info("In pdf_splitter_create_project_post()")
+
+    project_title   = request.form['project_title']
+    project_action  = request.form['action']
+    project_file_name = None
+    project_file_bytes = None
+    project_file_size = 0
+    
+    if len(request.files) > 0:
+        project_file = request.files['project_file']
+        project_file_name = project_file.filename
+        project_file_bytes = request.files['project_file'].stream.read()
+        project_file_size = len(project_file_bytes)
+
+    db = hci_db()
+    # db.find_project('PUBLIC', project_title)
+    project_id = db.add_project('PUBLIC', project_title, project_file_name, project_file_bytes, project_file_size)
+
+    # resp = make_response(view(view_model, view_path="site/webroot_get.html"))
+    if project_id is not None:
+        return redirect("/pdf-splitter/project/my-project-id")
+    
+    view_model = get_model()
+    view_model["project_title"] = project_title
+    view_model["error"] = "Project title exists"
+    return view(view_model, view_path="pdf_splitter/pdf_splitter_create_project_get.html")
+    #return view(view_model)
 
 @app.route('/pdf-splitter/project/<project_id>')
 def pdf_splitter_project_get(project_id = None):
