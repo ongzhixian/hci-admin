7ade0e9c9a6777d3375c9c0f2b9a8861182708db
Ong Zhixian <zhixian@hotmail.com>
2020-12-05 09:14:31 +0800

    update deploy.ps1

--- website/deploy.ps1
+++ website/deploy.ps1
@@ -1,1 +1,31 @@
-gcloud app deploy --version=1 --quiet --no-promote
+param([string]$envType="test") 
+
+switch ($envType)
+{
+    "test" {
+        Write-Host "Deploying to test environment (no-promote)"
+        & gcloud app deploy --version=1 --quiet --no-promote
+        break
+    }
+
+    "prod" {
+        Write-Host "Deploying to production environment"
+        & gcloud app deploy --version=1 --quiet
+        break
+    }
+
+    "clean" {
+        Write-Host "Clean and remove deployment artifacts bucket"
+        & gsutil rm -r gs://asia.artifacts.hci-admin.appspot.com
+        break
+    }
+
+    "logs" {
+        & gcloud app logs tail -s default
+        break
+    }
+
+    default {
+        Write-Error "No setup found for $envType"
+    }
+}
